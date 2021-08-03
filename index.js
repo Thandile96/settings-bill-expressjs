ead5fd25d628dc4f7d1963f85e2b7691a46159f8
Athandile_Nkcaza <nkcazaathandile@gmail.com>
2021-08-03 22:46:09 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const exphbs  = require('express-handlebars');
+const bodyParser = require('body-parser');
+const SettingsBill = require('./settings-bill');
+
+const app = express();
+const settingsBill = SettingsBill();
+
+app.engine('handlebars', exphbs({defaultLayout: ''})); //configure express handlebars
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/', function(req, res){ //show homescreen
+    res.render('index', {
+        settings: settingsBill.getSettings(),
+        totals: settingsBill.totals()
+    });
+});
+
+app.post('/settings', function(req, res){//set the settings
+    console.log(req.body);
+
+    settingsBill.setSettings({
+        callCost: req.body.callCost,
+        smsCost: req.body.smsCost,
+        warningLevel: req.body.warningLevel,
+        criticalLevel: req.body.criticalLevel
+    });
+
+    res.redirect('/');
+})
+
+app.post('/action', function(req, res){ //record action for sms or call
+   
+   //capture the call type to add
+    settingsBill.recordAction(req.body.actionType);
+
+    res.redirect('/');
+});
+
+app.get('/actions', function(req, res){ //show all the actions
+    res.render('actions', {actions: settingsBill.actions() });
+
+});
+
+app.get('/actions:actionType', function(req, res){ //display all the sms or call actions
+    const actionType = req.params.actionType;
+    res.render('actions', {actions: settingsBill.actionsFor(actionType) });
+
+});
+
+const PORT = process.env.PORT || 3011; // made port number configurable using an env variable
+
+app.listen(PORT, function(){
+    console.log("App started at port", PORT)
+});
